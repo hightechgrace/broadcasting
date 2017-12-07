d2a2a926935480eea9fc501a781019a7c264ec6f
Micah Elizabeth Scott <micah@misc.name>
2017-12-06 17:00:21 -0800

    Transcode script for my windows workstation
    Premiere is having trouble with some (probably slightly broken) files, and
    this seems to be the quickest way to get them into prores. Uses GPU decode
    and encodes on all cores.

---
+++ brassica/bin/prores-transcode.cmd
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set DEST=F:\Transcode
+set ACCEL=-hwaccel dxva2
+set OPTS=-y -codec:v prores -profile:v 3 -qscale:v 12 -vendor ap10 -pix_fmt yuv422p10le
+
+for %%i in (%1 %2 %3 %4 %5 %6 %7 %8 %9) do (
+	set SRC=%%i
+	set TMP="%DEST%\temp-%%~ni.mov"
+	set FINAL="%DEST%\%%~ni.mov"
+	ffmpeg %ACCEL% -i !SRC! %OPTS% !TMP! && move !TMP! !FINAL! && echo Wrote !FINAL!
+)
