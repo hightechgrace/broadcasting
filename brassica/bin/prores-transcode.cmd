a54bf48194493433d48da0766cfe16075015ec73
Micah Elizabeth Scott <micah@misc.name>
2017-12-06 17:09:29 -0800

    Remove ineffective option

--- brassica/bin/prores-transcode.cmd
+++ brassica/bin/prores-transcode.cmd
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 set DEST=F:\Transcode
 set ACCEL=-hwaccel dxva2
-set OPTS=-y -codec:v prores -profile:v 3 -qscale:v 12 -vendor ap10 -pix_fmt yuv422p10le
+set OPTS=-y -codec:v prores -profile:v 3 -qscale:v 12 -pix_fmt yuv422p10le
 
 for %%i in (%1 %2 %3 %4 %5 %6 %7 %8 %9) do (
 	set SRC=%%i
